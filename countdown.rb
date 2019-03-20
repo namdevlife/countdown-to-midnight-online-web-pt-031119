db431d4c18b1659a4250e403140303326a1d9e95
NAM NGUYEN <namdevlife@gmail.com>
2019-03-20 06:28:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  return "HAPPY NEW YEAR"
+  return "HAPPY NEW YEAR!"
 end
 
 countdown (10)
@@ -16,7 +16,7 @@ def countdown_with_sleep(number)
     puts "#{number} SECOND(S)!"
     number -= 1
     end
-return "HAPPY NEW YEAR"
+return "HAPPY NEW YEAR!"
 end
 
 countdown(10)
