e13969bfe8d5e669f663764ee78c63bf584d7e8f
NAM NGUYEN <namdevlife@gmail.com>
2019-03-20 06:03:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  number = 0
+  while number < 10
+    puts "#{number} SECOND(S)!"
+    number += 1
+    sleep 1
+  end
+
+puts countdown "HAPPY NEW YEAR!"  
+
 end
